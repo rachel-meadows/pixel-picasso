8634179e3b5b195eba27afab543d5109d5340a01
thongchai-hon <hon.man@hotmail.com>
2022-04-22 14:58:35 +1200

    add submit function part 1

--- client/components/Form.jsx
+++ client/components/Form.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Form(props) {
+
+  const {pixels} = props
+  const [title, setTitle] = useState('')
+
+  function handleChange (event) {
+    setTitle(event.target.value)
+  }
+
   function handleSubmit(event) {
     event.preventDefault()
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input />
+    <form onSubmit={handleSubmit} >
+      <input onChange={handleChange} value = {title}/>
       Title:
       <button>Submit! </button>
     </form>
