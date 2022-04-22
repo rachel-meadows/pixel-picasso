480a7adb609aa594c72d1e51a5628426d5958ba0
atmour-tai-mourie <taii.mourie@gmail.com>
2022-04-22 12:28:33 +1200

    fix browser router import

--- client/index.js
+++ client/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux'
 import store from './store'
 
 import App from './components/App'
-import { Router } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
