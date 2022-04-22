d7844126f9ae28611bda186fa8486174b3c397f8
atmour-tai-mourie <taii.mourie@gmail.com>
2022-04-22 13:03:49 +1200

    add header

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -3,14 +3,15 @@ import React from 'react'
 function Header() {
   return (
     <header>
+      <ul className="nav__list">
+        <li className="nav__list__title">Home</li>
+      </ul>
+
       <h1 className="mainTitle">Pixel Picasso</h1>
-      <div className="nav">
-        <ul className="nav__list">
-          <li className="nav__list__title">Home</li>
-          <li className="nav__list__title">Gallery</li>
-          <li className="nav__list__title">About</li>
-        </ul>
-      </div>
+      <ul className="nav__list">
+        <li className="nav__list__title">Gallery</li>
+        <li className="nav__list__title">About</li>
+      </ul>
     </header>
   )
 }
